8becbe5a5da30922d17ff646f3c5ebe93d046d97
zeroistwo <jon061512@gmail.com>
2018-12-06 18:25:12 +0900

    report 페이지만으로 크롤링할 수 있게 전반적인 수정

--- MatchReport.py
+++ MatchReport.py
@@ -5,45 +5,41 @@ import datetime
 import helper.crawlerCommon as crawlerCommon
 
 # URL 변수
-URL = "http://www.kleague.com/schedule/get_lists?datatype=html&month="
-MATCHCENTERURL = "http://www.kleague.com/match?vw=record&gs_idx="
-SELECTLEAGUE = "&select_league="
-SELECTLEAGUEYEAR = "&select_league_year=2018"
 REPORTURL = "http://portal.kleague.com/common/result/result0051popup.do?"
 IPTYEAR = "iptMeetYear=2018"
 IPTGAMEID = "&iptGameid="
 IPTSEQ = "&iptMeetSeq="
 
-# 기타 변수
+# 기타 변수 - 년도나 K1, K2 경기 수 정보는 매년 수정 필요
 MONTH = 12
+YEAR = 2018
+K1_number = 228
+K2_number = 182
 CONSOLEGUIDE = "Input league number(league_num 1:K1, 2:K2):  "
-DATAFRAME = ['Match_ID', 'League', 'Round', 'Date', 'Time', 'First_Half_Start_Time', 'First_Half_End_Time', 'Half_Time', 'Second_Half_Start_Time', 'Second_Half_End_Time', 'Stadium', 'Home_Team', 'Home_Score', 'Away_Score', 'Away_Team']
+DATAFRAME = ['Match_Number', 'League', 'Date', 'Time', 'First_Half_Start_Time', 'First_Half_End_Time', 'Half_Time', 'Second_Half_Start_Time', 'Second_Half_End_Time', 'Stadium', 'Home_Team', 'Home_Score', 'Away_Score', 'Away_Team']
 FILENAME = "MatchReport"
 
-def getData(soup, league_num, league_str, match_list, number_match, data):
-    gs_idxList = []
-    for i in range(number_match):
-        idxList = []
-        idxList.append(match_list[i].get('gs_idx'))
-        gs_idxList.append(idxList)
-
-    # 공통
-    for j in tqdm(range(number_match)):  # 한번에 크롤링할 페이지 수를 설정해줄 수 있음
+def getData(league_num, league_str, data):
+    if league_num == "1":
+        game_number = K1_number
+    elif league_num == "2":
+        game_number = K2_number
+    for i in tqdm(range(game_number)):  # 한번에 크롤링할 페이지 수를 설정해줄 수 있음
         try:
             row_data = []
-            match_id = gs_idxList[j][0]
-            html = urlopen(MATCHCENTERURL + match_id).read()   # 각 매치센터 페이지 사이 url 입력
-            body = bs(html, 'lxml').body    # beautifulsoup 라이브러리를 통해 html을 전부 읽어오는 작업 수행
             if league_num == "1":
-                report_html = urlopen(REPORTURL + IPTYEAR + IPTGAMEID + match_id + IPTSEQ + league_num).read()  # 데이터 보고서 페이지 url
+                report_html = urlopen(REPORTURL + IPTYEAR + IPTGAMEID + str(i+1) + IPTSEQ + league_num).read()  # 데이터 보고서 페이지 url
             elif league_num == "2":
-                report_html = urlopen(REPORTURL + IPTYEAR + IPTGAMEID + str(int(match_id)-198) + IPTSEQ + league_num).read()  # 데이터 보고서 페이지 url
+                report_html = urlopen(REPORTURL + IPTYEAR + IPTGAMEID + str(i+1) + IPTSEQ + league_num).read()  # 데이터 보고서 페이지 url
             else:
                 pass
             report = bs(report_html, 'lxml').body   # beautifulsoup 라이브러리를 통해 html을 전부 읽어오는 작업 수행
-            date = body.findAll('div', class_="match-info")[0].get_text().split(" / ")[0].split(" (")[0]  # 경기날짜 YYYY-MM-DD
-            hour = int(body.findAll('div', class_="match-info")[0].get_text().split(" / ")[0].split(")  ")[1].split(":")[0])  # 시
-            minute = int(body.findAll('div', class_="match-info")[0].get_text().split(" / ")[0].split(")  ")[1].split(":")[1])  # 분
+            month = int(report.findAll('td', class_='bar_bottm_right')[0].get_text().split(' ')[1].split('/')[0])
+            date = int(report.findAll('td', class_='bar_bottm_right')[0].get_text().split(' ')[1].split('/')[1].split('(')[0])
+            hour = int(report.findAll('td', class_='bar_bottm_right')[0].get_text().split(' ')[2].split(':')[0])  # 시
+            minute = int(report.findAll('td', class_='bar_bottm_right')[0].get_text().split(' ')[2].split(':')[1])  # 분
+
+            day = datetime.date(YEAR, month, date)
             time = datetime.time(hour, minute)  # 경기 시간
             first_form = report.findAll('td', class_='bar_bottm_right')[1].get_text().split(' ~ ')
             second_form = report.findAll('td', class_='bar_bottm_right')[7].get_text().split(' ~ ')
@@ -61,21 +57,20 @@ def getData(soup, league_num, league_str, match_list, number_match, data):
             second_half_start_time = datetime.time(second_start_hour, second_start_minute)
             second_half_end_time = datetime.time(second_end_hour, second_end_minute)
 
-            row_data.append(match_id)                                                                               # 1. Match_ID
-            row_data.append(league_str)                                                                             # 2. League
-            row_data.append(soup.findAll('td', class_="col-rd")[j].get_text().split("R")[0])                        # 3. Round
-            row_data.append(date)                                                                                   # 4. Date YYYY-MM-DD
-            row_data.append(time.isoformat())                                                                       # 5. Time HH-mm-SS
-            row_data.append(first_half_start_time)                                                                  # 6. First_Hlaf_Start_Time
-            row_data.append(first_half_end_time)                                                                    # 7. First_Half_End_Time
-            row_data.append(half_time)                                                                              # 8. Half_Time
-            row_data.append(second_half_start_time)                                                                 # 9. Second_Half_Start_Time
-            row_data.append(second_half_end_time)                                                                   # 10. Second_Half_End_Time
-            row_data.append(body.findAll('div', class_="match-info")[0].get_text().split(" / ")[2].split("\r")[0])  # 11. Stadium
-            row_data.append(body.findAll('div', class_="team-1")[0].get_text())                                     # 12. Home_Team
-            row_data.append(body.findAll('div', class_="score")[0].get_text().split("\n")[0].split(" : ")[0])       # 13. Home_Score
-            row_data.append(body.findAll('div', class_="score")[0].get_text().split("\n")[0].split(" : ")[1])       # 14. Away_Score
-            row_data.append(body.findAll('div', class_="team-2")[0].get_text())                                     # 15. Away_Team
+            row_data.append(i+1)                                                                                            # 1. Match_ID
+            row_data.append(league_str)                                                                                     # 2. League
+            row_data.append(day)                                                                                            # 3. Date YYYY-MM-DD
+            row_data.append(time.isoformat())                                                                               # 4. Time HH-mm-SS
+            row_data.append(first_half_start_time)                                                                          # 5. First_Hlaf_Start_Time
+            row_data.append(first_half_end_time)                                                                            # 6. First_Half_End_Time
+            row_data.append(half_time)                                                                                      # 7. Half_Time
+            row_data.append(second_half_start_time)                                                                         # 8. Second_Half_Start_Time
+            row_data.append(second_half_end_time)                                                                           # 9. Second_Half_End_Time
+            row_data.append(report.findAll('tr')[2].findAll('td', class_='bar_bottm_right')[0].get_text().split(' / ')[0])  # 10. Stadium
+            row_data.append(report.find('table', class_='height110 border01 mb20').findAll('tr')[0].findAll('td')[8].p.get_text())  # 11. Home_Team
+            row_data.append(report.find('table', class_='height110 border01 mb20').findAll('tr')[0].findAll('td')[10].get_text())   # 12. Home_Score
+            row_data.append(report.find('table', class_='height110 border01 mb20').findAll('tr')[0].findAll('td')[16].get_text())   # 13. Away_Score
+            row_data.append(report.find('table', class_='height110 border01 mb20').findAll('tr')[0].findAll('td')[17].p.get_text()) # 14. Away_Team
             data.append(row_data)
 
         except Exception as e:
@@ -86,16 +81,9 @@ def getData(soup, league_num, league_str, match_list, number_match, data):
 def setBasicInfo(league_num, league_str):
     # league_num 1:K1, 2:K2 98:R, 99:ACL
     result = []
-    for n in range(MONTH):
-        url = urlopen(URL + str(n + 1).zfill(2) + SELECTLEAGUE + league_num + SELECTLEAGUEYEAR).read()  # 크롤링하고자 하는 사이트 url명을 입력
-        soup = bs(url, 'lxml').body  # beautifulsoup 라이브러리를 통해 html을 전부 읽어오는 작업 수행
-
-        match_list = crawlerCommon.getButtonList(soup, league_str)
-        number_match = len(match_list)
-
-        data = []
-        data = getData(soup, league_num, league_str, match_list, number_match, data)
-        result.extend(data)
+    data = []
+    data = getData(league_num, league_str, data)
+    result.extend(data)
     return result
 
 def crawlMatchCenter():
